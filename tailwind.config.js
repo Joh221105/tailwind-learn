f9aa22e591dbd42ef8b27b194634575af0826003
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-10-24 14:23:46 -0400

    lesson 3 completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     './src/**/*.{html,js}',  
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)'}
+      }
+    },
   },
   plugins: [],
 }
